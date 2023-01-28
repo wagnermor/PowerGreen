bfa4777537181be430ad434d927904b5d49773a1
WagnerMor <wagnermor@outlook.com>
2023-01-28 17:20:41 -0300

    Fix: Start spring boot project

---
+++ src/test/java/org/generation/powerGreen/PowerGreenEnergiaLimpaEmMovimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.powerGreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerGreenEnergiaLimpaEmMovimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
